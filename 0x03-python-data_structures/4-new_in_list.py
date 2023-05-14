e595ddfcb156e54f9b004c1fb061544a4e5c0a40
MoMoAxe <tobioluwa348@gmail.com>
2023-05-14 16:03:56 -0700

    4-new_in_list.py4-new_in_list.py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list
+    if idx >= len(my_list):
+        return my_list
+    else:
+        temp = my_list[:]
+        temp[idx] = element
+        return temp
