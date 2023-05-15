949b4f38a67ed12ee04e7c229334f5b5a08f40c5
MoMoAxe <tobioluwa348@gmail.com>
2023-05-15 15:51:06 -0700

    6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        row_len = len(row)
+        for i in range(row_len):
+            if i != row_len - 1:
+                print("{:d}".format(row[i]), end=' ')
+            else:
+                print("{:d}".format(row[i]), end='')
+        print()
