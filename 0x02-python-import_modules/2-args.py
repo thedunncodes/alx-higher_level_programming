fe4c35da063cd04351122c04ef74d59b845afad8
MoMoAxe <tobioluwa348@gmail.com>
2023-05-16 06:11:21 -0700

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+from sys import argv
+
+if __name__ != "__main__":
+    exit()
+
+if len(argv) <= 1:
+    print("{} argument".format(len(argv) - 1))
+else:
+    if len(argv) <= 2:
+        print("{} argument:".format(len(argv) - 1))
+    else:
+        print("{} arguments:".format(len(argv) - 1))
+for i in range(1, len(argv)):
+    print("{}: {}".format(i, argv[i]))
