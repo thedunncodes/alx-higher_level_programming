9e6d68d2cf7a304f20605a703d808535ac556c2c
MoMoAxe <tobioluwa348@gmail.com>
2023-05-29 05:08:20 -0700

    0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,17 @@
+try:
+    def safe_print_list(my_list=[], x=0):
+        count = 0
+        for item in my_list:
+            count += 1
+        if not (x > count):
+            for p in range(x):
+                print(my_list[p], end="")
+            print()
+            return x
+        else:
+            for i in range(count):
+                print(my_list[i], end="")
+            print()
+            return (count)
+except IndexError:
+    print("Error...")
