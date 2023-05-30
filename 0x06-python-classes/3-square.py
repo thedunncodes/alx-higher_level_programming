2db25c649d6d84f41121721f43fa01a6ef9bddf5
MoMoAxe <tobioluwa348@gmail.com>
2023-05-30 14:20:54 -0700

    3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -25,8 +25,10 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-        def area(self):
-            """
-            this is a public instance method, returns curr sqr area
-            """
-            return self.__size ** 2
+    def area(self):
+        """
+        this is a public instance method, returns curr sqr area
+        """
+        return self.__size ** 2
+
+
