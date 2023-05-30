f5ffcb79e191b8dc05bd11d210cc4b175a64a9bf
MoMoAxe <tobioluwa348@gmail.com>
2023-05-30 13:56:09 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Square classs"""
+
+
+class Square:
+    """private instance attribute for Square"""
+    def __init__(self, size):
+        """Initialize method"""
+        self.__size = size
