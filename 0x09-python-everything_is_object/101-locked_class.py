f1854b212ec39d84f00e0e13fc38c66961599997
MoMoAxe <tobioluwa348@gmail.com>
2023-06-07 15:29:14 -0700

    101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -8,6 +8,3 @@ class LockedClass:
     """A class that only lets the user to create the instance
        attribute 'first_name'"""
     __slots__ = ['first_name']
-
-    def __init__(self, first_name):
-        LockedClass.first_name = first_name
