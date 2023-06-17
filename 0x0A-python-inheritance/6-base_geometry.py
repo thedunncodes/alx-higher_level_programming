aff2b7bc833d79e0f9151f1be88e4e288a903867
MoMoAxe <tobioluwa348@gmail.com>
2023-06-17 13:12:44 -0700

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+
+"""
+BaseGeometry update
+"""
+
+
+class BaseGeometry:
+    """
+    This BaseGeometry class defines the basic structure for geometric calc,
+    this is updated to raise exception
+
+    Methods:
+    - area(): To raise an exception and should be implemented in derived classes.
+    """
+
+    @classmethod
+    def area(self):
+        """
+        This calculates the area of the geometric shape.
+
+        This method is intended to be overridden by derived classes to
+        provide the specific implementation for calculating the area of
+        the corresponding geometric shape.
+
+        Raises:
+        Exception: An exception is raised when called.
+        """
+        raise Exception("area() is not implemented")
