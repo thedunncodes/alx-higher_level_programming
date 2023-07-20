455b3ceac3eaacadde2f342ad2523620a7e96d5a
MoMoAxe <tobioluwa348@gmail.com>
2023-07-20 10:25:30 -0700

    Last 2 mandatory tasks

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,6 @@
+-- This lists the number of records with the same score in the 'second_table' table
+-- Result should display the score and the number of records with the label 'number'
+SELECT score, COUNT(*) AS number
+FROM second_table
+GROUP BY score
+ORDER BY number DESC
---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,6 @@
+-- This lists socre and names from second_table
+-- where name is not empty or null
+-- and orders them by score in a descending order
+SELECT score, name FROM second_table
+WHERE name != "" AND name IS NOT NULL
+ORDER BY score DESC;
