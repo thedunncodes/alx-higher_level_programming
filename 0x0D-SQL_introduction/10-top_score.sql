df353278390e834bfdd02d75d43e5f1acdf2667d
MoMoAxe <tobioluwa348@gmail.com>
2023-07-20 10:10:08 -0700

    Next 4 tasks after task 6

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- This lists all records of the table 'second_table' based on values of score.
+-- Records should be sorted in descending order of score.
+SELECT score, name FROM second_table ORDER BY score DESC;
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,2 @@
+-- Displays number of records in the table 'first_table' where id = 89
+SELECT COUNT(*) FROM first_table WHERE id = 89;
