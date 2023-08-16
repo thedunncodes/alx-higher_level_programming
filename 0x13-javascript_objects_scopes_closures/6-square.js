a39eb71f5eb04f44cbd58e8c2f8a2bf9a0c159b0
MoMoAxe <tobioluwa348@gmail.com>
2023-08-16 14:50:34 -0700

    o-s-c

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,55 +1,12 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      let row = '';
-      for (let j = 0; j < this.width; j++) {
-        row += 'X';
-      }
-      console.log(row);
-    }
-  }
-
-  rotate () {
-    const temp = this.height;
-    this.height = this.width;
-    this.width = temp;
-  }
-
-  double () {
-    this.width = this.width * 2;
-    this.height = this.height * 2;
-  }
-}
-
-class Square extends Rectangle {
-  constructor (size) {
-    super(size);
-    this.width = size;
-    this.height = size;
-  }
-
+const square = require('./5-square');
+class Square extends square {
   charPrint (c) {
-    for (let i = 0; i < this.height; i++) {
-      let row = '';
-      for (let j = 0; j < this.width; j++) {
-        if (c !== undefined) {
-          const Char = c;
-          row += Char;
-        } else {
-          const Char = 'X';
-          row += Char;
-        }
-      }
-      console.log(row);
+    if (!c) {
+      c = 'X';
     }
+    this.print(c);
   }
 }
+
 module.exports = Square;
