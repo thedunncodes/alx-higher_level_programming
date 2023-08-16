aa5bb0a5baa47a11eb96e80c2aa33fc0a2fc9a12
MoMoAxe <tobioluwa348@gmail.com>
2023-08-16 14:55:37 -0700

    o-s-c

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,10 +2,19 @@
 const square = require('./5-square');
 class Square extends square {
   charPrint (c) {
-    if (!c) {
-      c = 'X';
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        if (c !== undefined) {
+          const Char = c;
+          row += Char;
+        } else {
+          const Char = 'X';
+          row += Char;
+        }
+      }
+      console.log(row);
     }
-    this.print(c);
   }
 }
 
