d30a9f14b44f012a39b0fe7b50880ac11fb4b7d4
MoMoAxe <tobioluwa348@gmail.com>
2023-11-30 12:53:18 -0800

    Python Zero: line fix

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,8 +2,10 @@
 
 """Peak finder function"""
 
+
 def find_peak(list_of_integers):
     """Finds peak in unsorted integers list"""
+
     if list_of_integers:
         list_of_integers.sort()
         return list_of_integers[-1]
